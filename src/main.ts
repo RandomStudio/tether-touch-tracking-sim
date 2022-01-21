03caad7bb0d400e92e5e2e2752a7784dfe41e269
Stephen Buchanan <stephen.buchanan@random.studio>
2022-01-21 12:37:12 +0100

    send position object

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { Output, TetherAgent } from "tether-agent";
 import { encode } from "@msgpack/msgpack";
+import { remap} from "@anselan/maprange";
 import './style.scss'
 
 const agent = new TetherAgent("trackingSim");
 
-const main = async(tetherHost: string | null) => {
+interface IDimensions {
+  w: number;
+  h: number;
+}
+
+const main = async(tetherHost: string | null, dimensions: IDimensions) => {
   console.log("Connect Tether @ ", tetherHost, "...");
   try {
     await agent.connect({ protocol: "ws", host: tetherHost || undefined, port: 15675, path: "/ws"}, false);
@@ -12,7 +18,7 @@ const main = async(tetherHost: string | null) => {
     console.error("Tether connect error:", e);
   }
 
-  const trackedObjects = await agent.createOutput("trackedObjects");
+  const trackedObjects = await agent.createOutput("trackedPoints");
 
   const interactionArea = document.getElementById("interaction-area") as HTMLDivElement;
 
@@ -27,12 +33,12 @@ const main = async(tetherHost: string | null) => {
   
         shadow.setPointerCapture(ev.pointerId);
 
-        sendTrackedObject(trackedObjects, ev.pointerId, ev.x, ev.y);
+        sendTrackedObject(trackedObjects, ev.pointerId, ev.x, ev.y, dimensions);
   
         shadow.addEventListener("pointermove", (ev) => {
           shadow.style.left = `${ev.x}px`;
           shadow.style.top = `${ev.y}px`;
-          sendTrackedObject(trackedObjects, ev.pointerId, ev.x, ev.y);
+          sendTrackedObject(trackedObjects, ev.pointerId, ev.x, ev.y, dimensions);
           });
   
         shadow.addEventListener("pointerup", (_ev) => {
@@ -45,12 +51,17 @@ const main = async(tetherHost: string | null) => {
 const urlParams = new URLSearchParams(window.location.search);
 const tetherHost = urlParams.get("tetherHost");
 
-const sendTrackedObject = (output: Output, id: number, x: number, y: number) =>  {
+const sendTrackedObject = (output: Output, id: number, inX: number, inY: number, inputDimensions: IDimensions) =>  {
+  const [x,y] = [
+    remap(inX, [0, inputDimensions.w], [0, 1]),
+    remap(inY, [0, inputDimensions.h], [0,1])
+  ];
+
   const m = {
-    id, x, y
+    id, position: {x, y}
   };
   output.publish(encode(m));
 }
 
-window.onload = () => { main(tetherHost); }
+window.onload = () => { main(tetherHost, { w: window.innerWidth, h: window.innerHeight}); }
 
