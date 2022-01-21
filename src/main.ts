303c590527d2aa3d7dade99a4dc723b1be48dbd7
Stephen Buchanan <stephen.buchanan@random.studio>
2022-01-21 10:14:59 +0100

    div shadow follows pointers

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,26 @@ import './style.scss'
 
 console.log("start");
 
+const interactionArea = document.getElementById("interaction-area") as HTMLDivElement;
+
+if (interactionArea) {
+  interactionArea.addEventListener("pointerdown", (ev) => {
+    const shadow = document.createElement("div");
+      shadow.classList.add("shadow");
+      shadow.style.left = `${ev.x}px`;
+      shadow.style.top = `${ev.y}px`;
+
+      interactionArea.appendChild(shadow);
+
+      shadow.setPointerCapture(ev.pointerId);
+
+      shadow.addEventListener("pointermove", (ev) => {
+        shadow.style.left = `${ev.x}px`;
+        shadow.style.top = `${ev.y}px`;
+      });
+
+      shadow.addEventListener("pointerup", (_ev) => {
+        interactionArea.removeChild(shadow);
+      });
+  })
+}
